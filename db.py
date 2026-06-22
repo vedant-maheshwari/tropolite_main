7709c77e7aecbebba96a37a1158ce639d1ee6ea1
vedant-maheshwari <vedantmaheshwari23@gmail.com>
2026-06-22 17:57:22 +0530

    updated db.py (price toggle push)

--- db.py
+++ db.py
@@ -8,15 +8,15 @@ load_dotenv('.env')
 DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASE_URL_MD = os.getenv("DATABASE_URL_MD")
 
-# DATABASE_ADMIN_URL = os.getenv("DATABASE_ADMIN_DATABASE_URL") or os.getenv("DATABASE_ADMIN_POSTGRES_URL") or os.getenv("DATABASE_ADMIN_PRISMA_DATABASE_URL")
+DATABASE_ADMIN_URL = os.getenv("DATABASE_ADMIN_DATABASE_URL") or os.getenv("DATABASE_ADMIN_POSTGRES_URL") or os.getenv("DATABASE_ADMIN_PRISMA_DATABASE_URL")
 
-# if DATABASE_ADMIN_URL and DATABASE_ADMIN_URL.startswith("postgres://"):
-#     DATABASE_ADMIN_URL = DATABASE_ADMIN_URL.replace("postgres://", "postgresql://", 1)
+if DATABASE_ADMIN_URL and DATABASE_ADMIN_URL.startswith("postgres://"):
+    DATABASE_ADMIN_URL = DATABASE_ADMIN_URL.replace("postgres://", "postgresql://", 1)
 
-# if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-#     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-DATABASE_ADMIN_URL = os.getenv("DATABASE_ADMIN_URL")
+# DATABASE_ADMIN_URL = os.getenv("DATABASE_ADMIN_URL")
 
 print(DATABASE_URL)
 print(DATABASE_URL_MD)
